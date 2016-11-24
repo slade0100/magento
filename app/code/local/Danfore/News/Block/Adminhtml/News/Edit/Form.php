f5a2af509e6e24e06d230906e41b909fd99311be
xiajinchen <1292806761@qq.com>
2016-11-24 17:25:18 +0800

    jing init

--- app/code/local/Danfore/News/Block/Adminhtml/News/Edit/Form.php
+++ app/code/local/Danfore/News/Block/Adminhtml/News/Edit/Form.php
@@ -2,7 +2,9 @@
 {
     protected function _prepareForm()
     {
-        $form = new Varien_Data_Form(array('id' => 'edit_form', 'action' => $this->getUrl('*/*/save', array('id' => $this->getRequest()->getParam('id'))), 'method' => 'post', 'enctype' => 'multipart/form-data'));
+        $form = new Varien_Data_Form(
+            array('id' => 'edit_form', 'action' => $this->getUrl('*/*/save',
+                array('id' => $this->getRequest()->getParam('id'))), 'method' => 'post', 'enctype' => 'multipart/form-data'));
         $form->setUseContainer(true);
         $this->setForm($form);
         return parent::_prepareForm();
--- app/code/local/Jing/Log/Block/Adminhtml/Log/Edit.php
+++ app/code/local/Jing/Log/Block/Adminhtml/Log/Edit.php
@@ -8,6 +8,22 @@ class Jing_Log_Block_Adminhtml_Log_Edit extends Mage_Adminhtml_Block_Widget_Form
         $this->_objectId = 'id';
         $this->_blockGroup = 'log';
         $this->_controller = 'adminhtml_log';
-        
+        $this->_updateButton('save', 'label', Mage::helper('log')->__('save Log'));
+        $this->_updateButton('delete', 'label', Mage::helper('log')->__('Delete Log'));
+        $this->_addButton('saveendcontinue',
+            array('label' => Mage::helper('adminhtml')->__('Save And Continue Edit'),
+                'onclick' => 'saveAndContinueEdit()', 'class' => 'save',), -100);
+        $this->_formScripts[] = "function toggleEditor() {
+            if (tinyMCE . getInstanceById('news_content') == null) {
+                tinyMCE . execCommand('mceAddControl', false, 'news_content');
+            } else {
+                tinyMCE . execCommand('mceRemoveControl', false, 'news_content');
+}
+        }
+function saveAndContinueEdit()
+{
+editForm . submit($('edit_form') . action + 'back/edit/');
+}
+ ";
     }
 }
